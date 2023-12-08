ac0235fc4f1f8107f4692f9ab9d1880cf46787a7
Gianfranco <giacocipo@gmail.com>
2023-12-07 21:33:52 -0300

    Bathymetric information represented

---
+++ js/Models/BathCell.js
@@ -0,0 +1,79 @@
+import * as THREE from "three";
+
+import {Object3d} from "./Object3d";
+
+export default class BathCell extends Object3d{
+    constructor(geoCellInfo, lod) {
+        let vertexIndex = 0;
+        let i = 0;
+        let point = 0;
+
+        const width = geoCellInfo['width'];
+        const raster = geoCellInfo['raster'];
+        const rasterBath = geoCellInfo['rasterBath'];
+
+        super();
+
+        let size_factor = 1;
+        if (lod > 0){
+            size_factor = Math.pow(2, lod);
+        }
+
+        this.geometry = new THREE.PlaneGeometry(1024/size_factor, 1024/size_factor, width - 1, width - 1);
+        this.geometry.rotateX(-Math.PI / 2);
+        let vertices = this.geometry.attributes.position.array;
+
+        this.waterGeommetry = new THREE.PlaneGeometry(1024/size_factor, 1024/size_factor, width - 1, width - 1);
+        this.waterGeommetry.rotateX(-Math.PI / 2);
+        let waterVertices = this.waterGeommetry.attributes.position.array;
+
+        const size = width*width
+        const data = new Uint8Array( 4 * size);
+
+
+        while (i < size) {
+            vertexIndex = i*3
+            const stride = i * 4;
+
+            data[ stride ] = 55;
+            data[ stride + 1 ] = 55;
+            data[ stride + 2 ] = 255;
+
+            if (rasterBath[point] > 0) {
+                vertices[vertexIndex + 1] = (raster[point] - rasterBath[point])*2;
+                waterVertices[vertexIndex + 1] = raster[point]*2;
+                data[ stride + 3 ] = 255;
+            } else {
+                vertices[vertexIndex + 1] = raster[point]*2;
+                waterVertices[vertexIndex + 1] = raster[point]*2;
+                data[ stride + 3 ] = 0;
+            }
+
+            point++;
+            i++;
+        }
+
+        this.geometry.computeVertexNormals();
+        this.waterGeommetry.computeVertexNormals();
+
+        console.log(data)
+
+        const texture = new THREE.DataTexture( data, width, width);
+        texture.needsUpdate = true;
+
+        texture.magFilter = THREE.LinearFilter;
+        texture.flipY = true;
+        texture.generateMipmaps = true;
+        texture.colorSpace = THREE.SRGBColorSpace;
+
+        this.waterMaterial = new THREE.MeshPhongMaterial({color: 0xffffff, map: texture, shininess: 0.3});
+        this.waterMaterial.transparent = true;
+        //this.waterMaterial.side = THREE.DoubleSide;
+        //this.waterMaterial.wireframe = true;
+        this.waterMesh = new THREE.Mesh(this.waterGeommetry, this.waterMaterial);
+
+        this.material = new THREE.MeshPhongMaterial( { color: 0xb57272, shininess: 0.8 } );
+        //this.material.side = THREE.DoubleSide;
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+    }
+}
--- main.js
+++ main.js
@@ -1,39 +1,90 @@
 import * as THREE from 'three';
-import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
-import {getBlock} from "./js/libs/CDBQuery/CDBQuery";
+import {getBlock, getElevation} from "./js/libs/CDBQuery/CDBQuery";
 import GeoCell from "./js/Models/GeoCell";
 import RenderArea from "./js/libs/RenderArea/RenderArea";
+import BathCell from "./js/Models/BathCell";
+import GUI from "lil-gui";
+import {MapControls} from "three/addons/controls/MapControls";
+import Stats from "three/addons/libs/stats.module";
 
 
-let camera, controls, scene, renderer;
+let camera, controls, scene, renderer, stats;
 const clock = new THREE.Clock();
 
 const renderArea = new RenderArea()
 
+console.log("Hola")
+
 async function init() {
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 10, 10000);
-    camera.position.set(0, 200, -0);
-    camera.lookAt(-100, 100, -400);
+    camera.position.set(353.5, 353.5, 353.5);
 
     scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xffe0e0);
+    scene.background = new THREE.Color(0xe0e0ff);
 
     renderer = new THREE.WebGLRenderer({antialias: true, logarithmicDepthBuffer: true});
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+
+    stats = new Stats();
+    document.body.appendChild( stats.dom );
+
+    const globalPlane = new THREE.Plane( new THREE.Vector3( - 1, 0, 0 ), 0.1 );
+    const Empty = Object.freeze( [] );
+    const globalPlanes = [ globalPlane ];
+    renderer.clippingPlanes = Empty;
+
+    const gui = new GUI(),
+        folderGlobal = gui.addFolder( 'Global Clipping' ),
+        propsGlobal = {
+
+            get 'Enabled'() {
+
+                return renderer.clippingPlanes !== Empty;
+
+            },
+            set 'Enabled'( v ) {
+
+                renderer.clippingPlanes = v ? globalPlanes : Empty;
+
+            },
+
+            get 'Plane'() {
+
+                return globalPlane.constant;
+
+            },
+            set 'Plane'( v ) {
+
+                globalPlane.constant = v;
+
+            }
+
+        };
+
+    folderGlobal.add( propsGlobal, 'Enabled' );
+    folderGlobal.add( propsGlobal, 'Plane', - 500, 500 );
+
 
     const light = new THREE.DirectionalLight(0xffffff, 0.7);
     light.position.set(100, 800, -800);
     scene.add(light);
 
     // Set controls
-    controls = new FirstPersonControls(camera, renderer.domElement);
-    controls.lookSpeed = 0.15;
-    controls.movementSpeed = 300;
-    controls.verticalMin = 1.0;
-    controls.verticalMax = 3.0;
+    controls = new MapControls( camera, renderer.domElement );
 
+    controls.enableDamping = true; // an animation loop is required when either damping or autorotation are enabled
+    controls.dampingFactor = 0.05;
+
+    controls.screenSpacePanning = false;
+
+    controls.minDistance = 100;
+    controls.maxDistance = 1000;
+
+    controls.maxPolarAngle = Math.PI / 2;
+
+    /*
     const lodBlock1 = {
         lat: "S33",
         lon: "W070",
@@ -97,9 +148,30 @@ async function init() {
 
     const terrain_5 = await generateTerrain(lodBlock5)
     terrain_5.setPosition(768,0,768)
-    scene.add(terrain_5.mesh);
+    scene.add(terrain_5.mesh);*/
+
+
+    const lodBlock = {
+        lat: "N62",
+        lon: "W162",
+        lod: "L00",
+        lodNum: 0,
+        uref: "U0",
+        rref: "R0"
+    }
+
+    const terrain = await generateBathCell(lodBlock)
+    scene.add(terrain.mesh);
+    scene.add(terrain.waterMesh);
 
     window.addEventListener('resize', onWindowResize);
+}
+
+async function generateBathCell(lodBlockInfo) {
+
+    const geoCellInfo = await getElevation(lodBlockInfo)
+
+    return new BathCell(geoCellInfo, lodBlockInfo.lodNum);
 }
 
 async function generateTerrain(lodBlockInfo) {
@@ -117,7 +189,10 @@ function animate() {
 function render() {
     controls.update( clock.getDelta() );
     renderArea.update(camera.position.x, camera.position.z)
+
+    stats.begin();
     renderer.render( scene, camera );
+    stats.end();
 }
 
 function onWindowResize() {
